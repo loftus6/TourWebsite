5583337c00f05447f330644c2f4d28b5f872cb5a
loftus6 <66481256+loftus6@users.noreply.github.com>
2025-02-13 11:39:37 -0800

    mvc template

---
+++ Models/SeedData.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using TourWebsite.Models;
+
+namespace TourWebsite.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new TourContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<TourContext>>()))
+        {
+            // Look for any tours.
+            if (context.TourSites.Any())
+            {
+                return;   // DB has been seeded
+            }
+            context.TourSites.AddRange(
+                new TourSite
+                {
+                    Title = "Place1",
+                    Description = "Test Descrip 1",
+                    Coordinates = [0,0,0]
+                },
+                new TourSite
+                {
+                    Title = "Place2",
+                    Description = "Test Descrip 2",
+                    Coordinates = [0, 0, 0.5431]
+                }
+
+            );
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Models/TourContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TourWebsite.Models
+{
+    public class TourContext : DbContext
+    {
+        public DbSet<TourSite> TourSites { get; set; }
+
+        public TourContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+    }
+}
